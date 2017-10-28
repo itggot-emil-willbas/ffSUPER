cf27f0946f57ab13da89eedf77bd4d349cfeffe0
Emil Willbas <emil@vildbas.com>
2017-10-29 00:13:15 +0200

    lite mer

--- app.rb
+++ app.rb
@@ -4,8 +4,12 @@ require 'sqlite3'
 get('/') do
 
     db = SQLite3::Database.new('./db/fast_forward_db.sqlite')
-    result = db.execute("SELECT name FROM courses")
-    result = result[0..-1]
+    result = db.execute("SELECT * FROM courses")
+    #result = result[0..-1]
+    #Vi vill skicka två vars, en med alla kurnamn och en med info om collections  (namn + course_id)?
+    print "REULT::"
+    p result
+
     erb(:index, locals:{kurser:result})
 end
 
@@ -18,5 +22,27 @@ get('/comments/:collectionid') do
     print "RESULT::"
     p result
 end
+
+#Hämta (via ajax), collections tillhörande courses
+get('/collections/:courseid') do
+    course_id = params[:courseid]
+    db = SQLite3::Database.new('./db/fast_forward_db.sqlite')
+    resultbanan = db.execute("SELECT id,collection_name FROM collections WHERE course_id = ?",course_id)
+    print "Result (course_id):"
+    p resultbanan
+
+    ##############HUR SKICKA EN ARRAY TILL index.erb??
+    #erb(:index, locals:{results:resultbanan})
+    
+    #erb(:index, locals:{returned:result})
+end
+
+get('/hello') do
+    
+
+    '42'
+end
+
+    
 
 
