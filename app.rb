41287b058e8e726d7450deb81ba4cf73569a52c5
Emil Willbas <emil@vildbas.com>
2017-11-04 00:18:27 +0100

    json klar + ladda samlingar
    json klar + ladda samlingar

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sqlite3'
+require 'json'
 
 get('/') do
 
@@ -15,37 +16,22 @@ end
 
 #Hämta alla kommentarer från colllection
 
-get('/comments/:collectionid') do
-    collection_id = params[:collectionid]
+get('/comments/?') do
+    collection_id = params["collection-select"]
     db = SQLite3::Database.new('./db/fast_forward_db.sqlite')
-    result = db.execute("SELECT text FROM commments WHERE collection_id IN (SELECT id FROM collections WHERE id = ?)",collection_id).first
+    result = db.execute("SELECT text FROM commments WHERE collection_id IN (SELECT id FROM collections WHERE id = ?)",collection_id)
     print "RESULT::"
     p result
+    erb(:comments, locals:{comments:result})
 end
 
 #Hämta (via ajax), collections tillhörande courses
 get('/collections/:courseid') do
     course_id = params[:courseid]
     db = SQLite3::Database.new('./db/fast_forward_db.sqlite')
-    resultbanan = db.execute("SELECT id,collection_name FROM collections WHERE course_id = ?",course_id)
-    print "Result (course_id):"
-    p resultbanan
-    options = ""
-    for coll in resultbanan do
-        options << "<option value=" + coll[0].to_s + ">" + coll[1].to_s + "</option>"
-    end
-
-    options
-    #resultbanan = resultbanan[1][1]
-    #meddelande = "<h1>Lite h1-text som skickas</h1><h2>"  + resultbanan + "</h2>"
-    #print "Meddelande:"
-    #p meddelande
-   # meddelande.to_s
-
-    ##############HUR SKICKA EN ARRAY TILL index.erb??
-    #erb(:index, locals:{results:resultbanan})
-    
-    #erb(:index, locals:{returned:result})
+    array = db.execute("SELECT id,collection_name FROM collections WHERE course_id = ?",course_id)
+    object = {object_first_key:array}
+return object.to_json  
 end
 
 get('/hello') do
